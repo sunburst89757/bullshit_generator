1160767edf04e349a5838a49ad0ca4f46a1479b0
sunburst89757 <3520279278@qq.com>
2022-09-21 11:11:28 +0800

    build(config): build a standard node typescript enviroment

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  env: {
+    node: true
+  },
+  extends: ["airbnb-base", "eslint:recommended", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module"
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    "no-console": "off"
+  }
+};
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+import fs from "fs";
+console.log(fs);
+export const test = () => {
+  console.log("test");
+};
