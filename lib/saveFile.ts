970179662c218456b9f53cba8ccf73f8d624070c
sunburst89757 <3520279278@qq.com>
2022-09-24 11:31:53 +0800

    fix(lib/savefile): 修复生成文件反复覆盖的问题

--- lib/saveFile.ts
+++ lib/saveFile.ts
@@ -1,11 +1,14 @@
+import dayjs from "dayjs";
 import { existsSync, mkdirSync, writeFileSync } from "fs";
 import { resolve } from "path";
+
 import { generateArticle, ITitle } from "./generateArticle";
 
 export default function saveFile(title: ITitle, min: number, max: number) {
   const outputDir = resolve(__dirname, "../output");
   //    路径拼接
-  const outputFile = resolve(outputDir, `${title}.txt`);
+  const time = dayjs().format("YYYY-MM-DD HH-mm-ss");
+  const outputFile = resolve(outputDir, `${title}${time}.txt`);
   //   文件夹不存在就创建文件夹
   if (!existsSync(outputDir)) {
     mkdirSync(outputDir);
